d64a326ed2228be568555c3b0fe97e66a2da6762
gonzaga95 <gonzagas.dudu@gmail.com>
2024-09-23 14:49:24 -0400

    fix metodo ordem decrescente. método main para testes

--- src/edu/dio/list/ex6OrdenaNumeros/OrdenacaoNumeros.java
+++ src/edu/dio/list/ex6OrdenaNumeros/OrdenacaoNumeros.java
@@ -21,7 +21,34 @@ public class OrdenacaoNumeros {
     }
 
     public List<Integer> ordenarDescendente() {
-        Collections.reverse(listaNumeros);
+        Collections.sort(listaNumeros, Collections.reverseOrder());
         return listaNumeros;
+    }
+
+    @Override
+    public String toString() {
+        return "OrdenacaoNumeros{" +
+                "listaNumeros=" + listaNumeros +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoNumeros ordenacaoNumeros = new OrdenacaoNumeros();
+        ordenacaoNumeros.adicionarNumero(2);
+        ordenacaoNumeros.adicionarNumero(1);
+        ordenacaoNumeros.adicionarNumero(4);
+        ordenacaoNumeros.adicionarNumero(3);
+        ordenacaoNumeros.adicionarNumero(5);
+
+        System.out.println(ordenacaoNumeros);
+
+        ordenacaoNumeros.ordenarAscendente();
+
+        System.out.println(ordenacaoNumeros);
+
+        ordenacaoNumeros.adicionarNumero(25);
+
+        ordenacaoNumeros.ordenarDescendente();
+        System.out.println(ordenacaoNumeros);
     }
 }
